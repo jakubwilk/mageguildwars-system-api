f287d0086d99462657e637a19bc9d4e1e0153833
jakubwilk <jw@jakubwilk.pl>
2024-02-06 15:18:30 +0100

    Switch auth methods from users controller to auth controller

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,49 @@
+import { Body, Controller, Get, Post, Put, Req, Res, UseGuards } from '@nestjs/common'
+import { ApiCookieAuth } from '@nestjs/swagger'
+import { Request, Response } from 'express'
+
+import { AuthService } from './auth.service'
+import { RefreshGuard } from './guards'
+import { AuthCookieNameEnum, CreateUserDTO, LoginUserDTO } from './models'
+import { DEFAULT_REFRESH_OPTIONS, setAuthorizationHeader } from './utils'
+
+@Controller('api/v1/auth')
+export class AuthController {
+  constructor(private readonly _authService: AuthService) {}
+
+  @Put()
+  async createAccount(@Body() userData: CreateUserDTO, @Res() res: Response) {
+    const {
+      tokens: { access, refresh },
+      user,
+    } = await this._authService.createAccount(userData)
+
+    return res
+      .set(setAuthorizationHeader(access))
+      .cookie(AuthCookieNameEnum.REFRESH, refresh, { ...DEFAULT_REFRESH_OPTIONS })
+      .json({ data: user })
+  }
+
+  @Post()
+  async loginAccount(@Body() userData: LoginUserDTO, @Res() res: Response) {
+    const {
+      tokens: { access, refresh },
+      user,
+    } = await this._authService.loginAccount(userData)
+
+    return res
+      .set(setAuthorizationHeader(access))
+      .cookie(AuthCookieNameEnum.REFRESH, refresh, { ...DEFAULT_REFRESH_OPTIONS })
+      .json({ data: user })
+  }
+
+  @UseGuards(RefreshGuard)
+  @Get()
+  @ApiCookieAuth('refresh')
+  async autoLoginAccount(@Req() req: Request) {
+    console.log('req.cookie', req.cookies)
+    console.log('req.headers', req.headers)
+
+    await this._authService.autoLoginAccount()
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,34 +1,8 @@
-import { Body, Controller, Post, Put, Res } from '@nestjs/common'
-import { Response } from 'express'
+import { Controller } from '@nestjs/common'
 
-import { AuthService } from '../auth/auth.service'
-
-import { CreateUserDTO, LoginUserDTO } from './models'
 import { UsersService } from './users.service'
 
 @Controller('api/v1/users')
 export class UsersController {
-  constructor(
-    private readonly _usersService: UsersService,
-    private readonly _authService: AuthService,
-  ) {}
-
-  @Put()
-  async create(@Body() createUser: CreateUserDTO, @Res() res: Response) {
-    const user = await this._usersService.createAsync(createUser)
-    const { authToken: access, refreshToken: refresh } = user
-    return await this._usersService.returnUserWithTokens(user, access, refresh, res)
-  }
-
-  @Post()
-  async login(@Body() loginUser: LoginUserDTO, @Res() res: Response) {
-    const { email, password } = loginUser
-    await this._usersService.isUserExist(email)
-    const user = await this._usersService.loginAsync(loginUser)
-    await this._authService.isUserPasswordCorrect(user.password, password)
-    const access = await this._authService.createAccessToken(user)
-    const refresh = await this._authService.createRefreshToken(user)
-    await this._usersService.updateUserTokens(email, access, refresh)
-    return await this._usersService.returnUserWithTokens(user, access, refresh, res)
-  }
+  constructor(private readonly _usersService: UsersService) {}
 }
